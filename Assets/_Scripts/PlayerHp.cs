9fabb0df626c23299c671dee1236abeb63fb137b
meangpu <51825872+meangpu@users.noreply.github.com>
2023-04-01 20:24:18 +0700

    hp code first version

--- Assets/_Scripts/PlayerHp.cs
+++ Assets/_Scripts/PlayerHp.cs
@@ -1,8 +1,36 @@
 using UnityEngine;
+using System;
 
-public class PlayerHp : MonoBehaviour
+public class PlayerHp : MonoBehaviour, IDamageable
 {
-    [SerializeField] int _maxHP;
+    [SerializeField] int _maxHP = 100;
     public int _currentHP { get; private set; }
+    public static Action<int, int> OnPlayerUpdateHp;
+
+    void Start()
+    {
+        ResetPlayerHP();
+    }
+
+    void ResetPlayerHP()
+    {
+        _currentHP = _maxHP;
+        OnPlayerUpdateHp?.Invoke(_currentHP, _maxHP);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _currentHP -= damage;
+        CheckIfPlayerDead();
+
+        OnPlayerUpdateHp?.Invoke(_currentHP, _maxHP);
+    }
+
+    void CheckIfPlayerDead()
+    {
+        if (_currentHP <= 0) Debug.Log("Player is dead");
+    }
+
+
 
 }
--- Assets/_Scripts/PlayerHpUiDisplay.cs
+++ Assets/_Scripts/PlayerHpUiDisplay.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class PlayerHpUiDisplay : MonoBehaviour
 {
@@ -8,4 +9,23 @@ public class PlayerHpUiDisplay : MonoBehaviour
     [SerializeField] TMP_Text _hpNow;
     [SerializeField] TMP_Text _hpMax;
 
+    void OnEnable()
+    {
+        PlayerHp.OnPlayerUpdateHp += UpdatePlayerHp;
+    }
+    void OnDisable()
+    {
+        PlayerHp.OnPlayerUpdateHp -= UpdatePlayerHp;
+    }
+
+    private void UpdatePlayerHp(int nowHp, int maxHp)
+    {
+        // in case max hp change it take that as well
+        _hpSlider.maxValue = maxHp;
+        _hpSlider.value = nowHp;
+
+        _hpNow.SetText(nowHp.ToString());
+        _hpMax.SetText(maxHp.ToString());
+
+    }
 }

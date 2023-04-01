429fb1a43e6d6ca2049db8cb2753ab060078843b
meangpu <51825872+meangpu@users.noreply.github.com>
2023-04-02 01:26:03 +0700

    better health animation

--- Assets/_Scripts/PlayerHpUiDisplay.cs
+++ Assets/_Scripts/PlayerHpUiDisplay.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using DG.Tweening;
 
 public class PlayerHpUiDisplay : MonoBehaviour
 {
     [SerializeField] Image _hpSlider;
     [SerializeField] TMP_Text _hpNow;
     [SerializeField] TMP_Text _hpMax;
+
+    Color _startColor;
+
+    [SerializeField] Color _takeDmgColor;
+    [SerializeField] float _animationDuration = 0.2f;
+    [SerializeField] Ease _easeType;
 
     void OnEnable()
     {
@@ -17,14 +24,29 @@ public class PlayerHpUiDisplay : MonoBehaviour
         PlayerHp.OnPlayerUpdateHp -= UpdatePlayerHp;
     }
 
+    void Start()
+    {
+        _startColor = _hpSlider.color;
+    }
+
     private void UpdatePlayerHp(int nowHp, int maxHp)
     {
-        // in case max hp change it take that as well
         float hpPercent = (float)nowHp / maxHp;
-        _hpSlider.fillAmount = hpPercent;
+
+        AnimateHpSlider(hpPercent);
 
         _hpNow.SetText(nowHp.ToString());
         _hpMax.SetText($"/{maxHp.ToString()}");
+
+    }
+
+    private void AnimateHpSlider(float hpPercent)
+    {
+        _hpSlider.DOFillAmount(hpPercent, _animationDuration).SetEase(_easeType);
+
+        var sequence = DOTween.Sequence();
+        sequence.Append(_hpSlider.DOColor(_takeDmgColor, _animationDuration).SetEase(_easeType));
+        sequence.Append(_hpSlider.DOColor(_startColor, _animationDuration).SetEase(_easeType));
 
     }
 }

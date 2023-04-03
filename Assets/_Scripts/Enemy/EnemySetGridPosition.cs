60f626ab2d02947344cb211d5a7ed39f02c88fe7
KillerzScrubbie <tutteptan@kkumail.com>
2023-04-03 20:57:21 +1000

    Refined how enemy walks up stairs

--- Assets/_Scripts/Enemy/EnemySetGridPosition.cs
+++ Assets/_Scripts/Enemy/EnemySetGridPosition.cs
@@ -4,9 +4,11 @@ public class EnemySetGridPosition : MonoBehaviour
 {
     [SerializeField] private Transform pathfinderBall;
 
+    private readonly float yOffset = 0.5f;
+
     private void Update()
     {
         Vector3 position = pathfinderBall.position;
-        transform.position = new Vector3(Mathf.RoundToInt(position.x), position.y, Mathf.RoundToInt(position.z));
+        transform.position = new Vector3(Mathf.RoundToInt(position.x), Mathf.RoundToInt(position.y) + yOffset, Mathf.RoundToInt(position.z));
     }
 }

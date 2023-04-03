01672f13888cf83a19d33f84c22276fb6a8106c3
KillerzScrubbie <tutteptan@kkumail.com>
2023-04-03 21:13:23 +1000

    Smoothing stairs for AI

--- Assets/_Scripts/Enemy/EnemySetGridPosition.cs
+++ Assets/_Scripts/Enemy/EnemySetGridPosition.cs
@@ -5,10 +5,14 @@ public class EnemySetGridPosition : MonoBehaviour
     [SerializeField] private Transform pathfinderBall;
 
     private readonly float yOffset = 0.5f;
+    private readonly float stairsOffset = 1f;
 
     private void Update()
     {
         Vector3 position = pathfinderBall.position;
-        transform.position = new Vector3(Mathf.RoundToInt(position.x), Mathf.RoundToInt(position.y) + yOffset, Mathf.RoundToInt(position.z));
+
+        float offset = position.y % 1 >= 0.134f & position.y % 1 <= 0.5f ? stairsOffset : yOffset ;
+
+        transform.position = new Vector3(Mathf.RoundToInt(position.x), Mathf.RoundToInt(position.y) + offset, Mathf.RoundToInt(position.z));
     }
 }

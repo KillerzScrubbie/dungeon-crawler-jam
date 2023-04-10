babfe4ddab8ee77ad59a7a14f007ca9d9cbb8de1
meangpu <51825872+meangpu@users.noreply.github.com>
2023-04-11 00:16:33 +0700

    make dmg popup scale

--- Assets/_Scripts/Combat/DamagePopup.cs
+++ Assets/_Scripts/Combat/DamagePopup.cs
@@ -4,16 +4,28 @@ using TMPro;
 public class DamagePopup : MonoBehaviour
 {
     [SerializeField] Vector3 _moveUpSpeed = new Vector3(0, 20f, 0);
-    [SerializeField] float _lifeDuration = 3;
+    [SerializeField] float _maxLifeDuration = 1;
+    float _lifeDuration;
     [SerializeField] float _disappearSpeed = .5f;
     [SerializeField] TMP_Text _txtScpt;
     [SerializeField] Color _txtCol;
+    [SerializeField] float _increaseScaleAmount = 1.12f;
+    [SerializeField] float _decreaseScaleAmount = .92f;
 
+    void Start()
+    {
+        _lifeDuration = _maxLifeDuration;
+    }
 
     void Update()
     {
         transform.position += _moveUpSpeed * Time.deltaTime;
         _lifeDuration -= Time.deltaTime;
+
+        if (_lifeDuration > _maxLifeDuration * .5f) transform.localScale += Vector3.one * _increaseScaleAmount * Time.deltaTime;
+        else transform.localScale -= Vector3.one * _decreaseScaleAmount * Time.deltaTime;
+
+
         if (_lifeDuration <= 0)
         {
             _txtCol.a -= _disappearSpeed * Time.deltaTime;

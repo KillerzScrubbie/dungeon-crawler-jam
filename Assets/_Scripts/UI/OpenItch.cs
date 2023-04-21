82ed09450640e06c2eb4b8184e97686f08d95c6e
KillerzScrubbie <tutteptan@kkumail.com>
2023-04-21 12:45:34 +1000

    Polishing main menu

---
+++ Assets/_Scripts/UI/OpenItch.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class OpenItch : MonoBehaviour
+{
+    public void OpenItchPage()
+    {
+        Application.OpenURL("https://killerzscrubbie.itch.io/chronoleaper");
+    }
+}
